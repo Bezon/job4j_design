e5d5f32f477a6758e6c7e492610df41dba93208d
Bezon <bezonbezon@yandex.ru>
2023-03-05 14:35:24 +0300

    7. Переопределить и equals и hashCode [#1002 #397639]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -3,6 +3,7 @@ package ru.job4j.collection;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -33,5 +34,20 @@ public class User {
         System.out.println("");
         System.out.printf("user2 - хэшкод: %s, хэш: %s, бакет: %s",
                 hashCode2, hash2, bucket2);
+        for (User user : map.keySet()) {
+            System.out.println(user);
+        }
+    }
+
+        public int hashCode() {
+        return  Objects.hash(name, children, birthday);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
     }
 }
