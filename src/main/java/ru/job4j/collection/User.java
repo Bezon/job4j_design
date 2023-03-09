32ead92062c2afadb02d45e4289124e4fc2c8782
Bezon <bezonbezon@yandex.ru>
2023-03-09 23:54:05 +0300

    тест

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -37,9 +37,20 @@ public class User {
         for (User user : map.keySet()) {
             System.out.println(user);
         }
+        System.out.println(binary(123456789));
     }
 
-        public int hashCode() {
+    public static String binary(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 32; i++) {
+            sb.append(num % 2 == 0 ? 0 : 1);
+            sb.append((i + 1) % 8 == 0 ? " " : "");
+            num /= 2;
+        }
+        return sb.reverse().toString();
+    }
+
+    public int hashCode() {
         return  Objects.hash(name, children, birthday);
     }
 
