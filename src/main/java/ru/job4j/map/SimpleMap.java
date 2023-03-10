0096bf32384785f5b0ec0a47ccd6f04cc951e29e
Bezon <bezonbezon@yandex.ru>
2023-03-10 15:39:09 +0300

    10. Реализовать собственную структуру данных - HashMap [#1008]

---
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -0,0 +1,63 @@
+package ru.job4j.map;
+
+import java.util.Iterator;
+
+public class SimpleMap<K, V> implements Map<K, V> {
+
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private int capacity = 8;
+
+    private int count = 0;
+
+    private int modCount = 0;
+
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+    @Override
+    public boolean put(K key, V value) {
+        count++;
+        modCount++;
+        return false;
+    }
+
+    private int hash(int hashCode) {
+        return (hashCode == 0) ? 0 : (hashCode) ^ (hashCode >>> 16);
+    }
+
+    private int indexFor(int hash) {
+        return hash & (table.length - 1);
+    }
+
+    private void expand() {
+
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        count--;
+        modCount++;
+        return false;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+
+    private static class MapEntry<K, V> {
+
+        K key;
+        V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
